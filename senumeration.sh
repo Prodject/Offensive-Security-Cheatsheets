bb875a60d9803de0e06339f236813ec5e0a0e0c0
root <root@localhost.localdomain>
2018-01-13 22:30:58 -0500

    housekeeping

--- senumeration.sh
+++ senumeration.sh
@@ -19,6 +19,10 @@ java -jar /usr/share/dirbuster/DirBuster-1.0-RC1.jar -e php,html -u http://$1:80
 # nmap $1 -vv -Pn -p1433 --script=ms-sql-info,ms-sql-config,ms-sql-dump-hashes --script-args=mssql.instance-port=1433,smsql.username-sa,mssql.password-sa -oN $1/$1-nmap-sql
 
 
+# KERBEROS *****************************************************************************************************
+# nmap $1 -vv -p88 --script krb5-enum-users --script-args krb5-enum-users.realm=htb,userdb=/usr/share/wordlists/names.txt -vv -oN /root/tools/$1/$1-kerberos-enum
+
+
 # SMB *****************************************************************************************************
 # https://monkeysm8.gitbooks.io/pentesting-methodology/common_portsservices_and_how_to_use_them/port_139_and_445-_smbsamba_shares.html
 # nmap $1 -vv -Pn -p445 --script=smb-enum-shares.nse,smb-ls.nse,smb-enum-users.nse,smb-mbenum.nse,smb-os-discovery.nse,smb-security-mode.nse,smb-vuln-cve2009-3103.nse,smb-vuln-ms06-025.nse,smb-vuln-ms07-029.nse,smb-vuln-ms08-067.nse,smb-vuln-ms10-054.nse,smb-vuln-ms10-061.nse,smb-vuln-regsvc-dos.nse,smbv2-enabled.nse -oN $1/$1-nmap-smb
