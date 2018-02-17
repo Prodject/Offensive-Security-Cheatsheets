c5f373a170873e6e82f7036b4fa54d49fc3689ec
root <root@localhost.localdomain>
2018-02-17 10:30:12 -0500

    housekeeping

--- se-http.sh
+++ se-http.sh
@@ -2,5 +2,8 @@
 mkdir $1 2>/dev/null
 
 nmap $1 -vv -Pn -p80,443 --script=http-vhosts,http-userdir-enum,http-apache-negotiation,http-backup-finder,http-config-backup,http-default-accounts,http-methods,http-method-tamper,http-passwd,http-robots.txt,http-devframework,http-enum,http-frontpage-login,http-git,http-iis-webdav-vuln,http-php-version,http-robots.txt,http-shellshock,http-vuln-cve2015-1635 -oN $1/$1-nmap-http
+
+# gobuster -u http://$1 -r -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -x php,txt -t 150 -l | tee /root/tools/$1/$1-gobuster
+
 nikto -h http://$1:80 | tee $1/$1-nikto
-# wpscan --url http://$1:80 --enumerate u | tee $1/$1-wpscan-users
+wpscan --url http://$1:80 --enumerate u,t,p | tee $1/$1-wpscan-enum
--- senumeration.sh
+++ senumeration.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 mkdir $1 2>/dev/null
 nmap $1 -vv -Pn -sV -A -oN $1/$1-nmap-quick
-java -jar /usr/share/dirbuster/DirBuster-1.0-RC1.jar -e php,html -u http://$1:80 -l /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt | tee $1/$1-dirbuster
+
+gobuster -u http://$1 -r -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -x php,txt -t 200 -l | tee /root/tools/$1/$1-gobuster
+
+#java -jar /usr/share/dirbuster/DirBuster-1.0-RC1.jar -e php,html -u http://$1:80 -l /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt | tee $1/$1-dirbuster
 
 # DNS *****************************************************************************************************
 # dig axfr @nameserver domain.net | tee $1/$1-dns-zone-transfer
