7347e2655d141c9f902bcacfca15e94bcc8877f0
root <root@localhost.localdomain>
2018-03-06 21:20:36 +0000

    housekeeping

--- se-sql.sh
+++ se-sql.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-mkdir $1 2>/dev/null
+mkdir -p /root/tools/$1 2>/dev/null
+resultsFolder=/root/tools/$1
 
-nmap $1 -vv -Pn -p1433 --script=ms-sql-info,ms-sql-config,ms-sql-dump-hashes --script-args=mssql.instance-port=1433,smsql.username-sa,mssql.password-sa -oN $1/$1-nmap-sql
-nmap $1 -vv -Pn -p1433 --script=ms-sql-tables --script-args=mssql.username=admin,mssql.password='pass',ms-sql-tables.maxtables='0' -oN $1/$1-nmap-sql-tables-databases
-nmap $1 -vv -Pn -p1433 --script=ms-sql-dump-hashes --script-args=mssql.username=admin,mssql.password='pass' -oN $1/$1-nmap-sql-dumped-hashes
-nmap $1 -vv -Pn -p1433 --script=ms-sql-config --script-args=mssql.username=admin,mssql.password='pass' -oN $1/$1-nmap-sql-config
+nmap $1 -vv -Pn -p1433 --script=ms-sql-info,ms-sql-config,ms-sql-dump-hashes --script-args=mssql.instance-port=1433,smsql.username-sa,mssql.password-sa -oN $resultsFolder/$1-nmap-sql
+nmap $1 -vv -Pn -p1433 --script=ms-sql-tables --script-args=mssql.username=admin,mssql.password='pass',ms-sql-tables.maxtables='0' -oN $resultsFolder/$1-nmap-sql-tables-databases
+nmap $1 -vv -Pn -p1433 --script=ms-sql-dump-hashes --script-args=mssql.username=admin,mssql.password='pass' -oN $resultsFolder/$1-nmap-sql-dumped-hashes
+nmap $1 -vv -Pn -p1433 --script=ms-sql-config --script-args=mssql.username=admin,mssql.password='pass' -oN $resultsFolder/$1-nmap-sql-config
