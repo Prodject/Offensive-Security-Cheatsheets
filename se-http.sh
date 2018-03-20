ce6d349a6d521782a14cf3b87bcaa2a4ed56b74e
root <root@localhost.localdomain>
2018-03-20 21:44:15 +0000

    housekeeping

--- se-http.sh
+++ se-http.sh
@@ -3,7 +3,7 @@ ip=`echo $1 | cut -d ":" -f1`
 mkdir -p /root/tools/$ip 2>/dev/null
 resultsFolder=/root/tools/$ip
 
-nmap $ip -vv -Pn -p80,443 --script=http-vhosts,http-userdir-enum,http-apache-negotiation,http-backup-finder,http-config-backup,http-default-accounts,http-methods,http-method-tamper,http-passwd,http-robots.txt,http-devframework,http-enum,http-frontpage-login,http-git,http-iis-webdav-vuln,http-webdav-scan,http-php-version,http-robots.txt,http-shellshock,http-vuln-cve2015-1635 -oN $resultsFolder/$1-nmap-http
-gobuster -u http://$1 -r -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -x asp,htm,html -t 170 -l | tee $resultsFolder/$1-gobuster
-nikto -h http://$1 | tee $resultsFolder/$1-nikto
+# nmap $ip -vv -Pn -p80,443 --script=http-vhosts,http-userdir-enum,http-apache-negotiation,http-backup-finder,http-config-backup,http-default-accounts,http-methods,http-method-tamper,http-passwd,http-robots.txt,http-devframework,http-enum,http-frontpage-login,http-git,http-iis-webdav-vuln,http-webdav-scan,http-php-version,http-robots.txt,http-shellshock,http-vuln-cve2015-1635 -oN $resultsFolder/$1-nmap-http
+gobuster -u https://$1 -r -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -x php,htm,html -t 200 -l | tee $resultsFolder/$1-gobuster
+nikto -h https://$1 | tee $resultsFolder/$1-nikto
 wpscan --url http://$1 --enumerate u,t,p | tee $resultsFolder/$1-wpscan-enum
--- senumeration.sh
+++ senumeration.sh
@@ -8,5 +8,4 @@ nmap $1 -vv -Pn -sV -A -oN $resultsFolder/$1-nmap-quick
 
 # NMAP extended - full and udp top 200 *****************************************************************************************************
 nmap $1 -vv -Pn -T4 -p- -oN $resultsFolder/$1-nmap-full
-# nmap $1 -vv -Pn -A -sC -sS -T4 -p- -oN $resultsFolder/$1-nmap-full
 nmap $1 -vv -Pn -A -sC -sU -T4 --top-ports 200 -oN $resultsFolder/$1-nmap-udp-top200
