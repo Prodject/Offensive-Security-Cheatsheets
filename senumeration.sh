e6adc82990c81de243b30fd379d7fd45e70cb9ac
root <root@localhost.localdomain>
2018-03-25 18:50:17 +0100

    housekeeping

--- senumeration.sh
+++ senumeration.sh
@@ -5,7 +5,5 @@ mkdir $resultsFolder/exploits
 mkdir $resultsFolder/loot
 
 nmap $1 -vv -Pn -sV -A -oN $resultsFolder/$1-nmap-quick
-
-# NMAP extended - full and udp top 200 *****************************************************************************************************
 nmap $1 -vv -Pn -T4 -p- -oN $resultsFolder/$1-nmap-full
-nmap $1 -vv -Pn -A -sC -sU -T4 --top-ports 200 -oN $resultsFolder/$1-nmap-udp-top200
+nmap $1 -vv -Pn -sU -T4 --top-ports 200 -oN $resultsFolder/$1-nmap-udp-top200
